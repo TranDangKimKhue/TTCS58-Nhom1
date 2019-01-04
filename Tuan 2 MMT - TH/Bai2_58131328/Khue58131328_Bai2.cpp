ff4315cf4efc54f6ead5e39951ddee71c96c1b9c
TranDangKimKhue <46096987+TranDangKimKhue@users.noreply.github.com>
2019-01-04 09:04:07 +0700

    Update Khue58131328_Bai2.cpp

--- Tuan 2 MMT - TH/Bai2_58131328/Khue58131328_Bai2.cpp
+++ Tuan 2 MMT - TH/Bai2_58131328/Khue58131328_Bai2.cpp
@@ -29,6 +29,7 @@ void nhap(nhanVien a[],int &n)
 }
 void inDanhSachNv(nhanVien a[],int n)
 {
+	cout<<"STT"<<"|"<<"Ma nhan vien"<<"|"<<"Ho ten nhan vien"<<"|"<<"Tuoi"<<"|"<<"Luong"<<endl;
 	for(int i=0;i<n;i++)
 	{
 		cout<<i+1<<"\t"<<a[i].maNv<<"\t"<<" "<<a[i].hoTen<<"\t\t    "<<a[i].tuoi<<"\t"<<a[i].luongCb<<endl;
@@ -37,25 +38,35 @@ void inDanhSachNv(nhanVien a[],int n)
 }
 void themNv(nhanVien a[],int &n)
 {
-	int x;
+	int x,index;
 	cout<<"Vi tri can them : ";	cin>>x;
 	if(x>=0&&x<=n)
 	{
-		for(int i=x;i<n;i++)
-			a[i+1]=a[i];
-		cout<<"Ma nhan vien : ";	cin>>a[x].maNv;
-		fflush(stdin);
-		cout<<"Ho ten nhan vien : ";	cin.getline(a[x].hoTen,30);
-		cout<<"Tuoi : ";	cin>>a[x].tuoi;
-		cout<<"Luong : ";	cin>>a[x].luongCb;
-		n++;
-		cout<<"Danh sach nhan vien sau khi them"<<endl;
-		k<<"Sau khi them mot nhan vien vao danh sach : "<<endl;
-		k<<"---------------------------------------------"<<endl;
-		k<<"STT"<<"|"<<"Ma nhan vien"<<"|"<<"Ho ten nhan vien"<<"|"<<"Tuoi"<<"|"<<"Luong"<<endl;
+		int h;
+		cout<<"Ma nhan vien : ";	cin>>h;
 		for(int j=0;j<n;j++)
-			k<<a[j].maNv<<"\t"<<" "<<a[j].hoTen<<"\t\t    "<<a[j].tuoi<<"\t"<<a[j].luongCb<<endl;
-		inDanhSachNv(a,n);
+			if(a[j].maNv==h)
+				index=j;
+		if(a[index].maNv==h)
+			cout<<"Ma nhan vien da ton tai!"<<endl;
+		else
+		{
+			for(int i=n;i>x;i--)
+				a[i]=a[i-1];
+			a[x].maNv=h;
+			fflush(stdin);
+			cout<<"Ho ten nhan vien : ";	cin.getline(a[x].hoTen,30);
+			cout<<"Tuoi : ";	cin>>a[x].tuoi;
+			cout<<"Luong : ";	cin>>a[x].luongCb;
+			n++;
+			cout<<"Danh sach nhan vien sau khi them"<<endl;
+			k<<"Sau khi them mot nhan vien vao danh sach : "<<endl;
+			k<<"---------------------------------------------"<<endl;
+			k<<"STT"<<"|"<<"Ma nhan vien"<<"|"<<"Ho ten nhan vien"<<"|"<<"Tuoi"<<"|"<<"Luong"<<endl;
+			for(int j=0;j<n;j++)
+				k<<a[j].maNv<<"\t"<<" "<<a[j].hoTen<<"\t\t    "<<a[j].tuoi<<"\t"<<a[j].luongCb<<endl;
+			inDanhSachNv(a,n);
+		}
 	}
 	else
 		cout<<"Vi tri nay khong the them!"<<endl;
@@ -67,15 +78,25 @@ void xoaNv(nhanVien a[],int &n)
 	cin>>x;
 	for(int i=0;i<n;i++)
 		if(a[i].maNv==x)
-			a[i]=a[i+1];
-	n--;
-	cout<<"Danh sach nhan vien sau khi xoa"<<endl;
-	k<<"Sau khi xoa mot nhan vien vao danh sach : "<<endl;
-	k<<"---------------------------------------------"<<endl;
-	k<<"STT"<<"|"<<"Ma nhan vien"<<"|"<<"Ho ten nhan vien"<<"|"<<"Tuoi"<<"|"<<"Luong"<<endl;
-	for(int j=0;j<n;j++)
-		k<<a[j].maNv<<"\t"<<" "<<a[j].hoTen<<"\t\t    "<<a[j].tuoi<<"\t"<<a[j].luongCb<<endl;
-	inDanhSachNv(a,n);
+			index=i;
+	if(a[index].maNv==x)
+	{
+		while(index<n)
+		{
+			a[index]=a[index+1];
+			index++;
+		}
+		n--;
+		cout<<"Danh sach nhan vien sau khi xoa"<<endl;
+		k<<"Sau khi xoa mot nhan vien vao danh sach : "<<endl;
+		k<<"---------------------------------------------"<<endl;
+		k<<"STT"<<"|"<<"Ma nhan vien"<<"|"<<"Ho ten nhan vien"<<"|"<<"Tuoi"<<"|"<<"Luong"<<endl;
+		for(int j=0;j<n;j++)
+			k<<a[j].maNv<<"\t"<<" "<<a[j].hoTen<<"\t\t    "<<a[j].tuoi<<"\t"<<a[j].luongCb<<endl;
+		inDanhSachNv(a,n);
+	}
+	else
+		cout<<"Ma nhan vien khong ton tai!"<<endl;
 }
 void capNhatNv(nhanVien a[],int n)
 {
@@ -85,21 +106,24 @@ void capNhatNv(nhanVien a[],int n)
 	for(int i=0;i<n;i++)
 	{
 		if(a[i].maNv==x)
-		{
 			index=i;
-			fflush(stdin);
-			cout<<"Ho ten nhan vien : ";	cin.getline(a[index].hoTen,30);
-			cout<<"Tuoi : ";	cin>>a[index].tuoi;
-			cout<<"Luong : ";	cin>>a[i].luongCb;
-		}
 	}
-	cout<<"Danh sach nhan vien sau khi cat nhap"<<endl;
-	k<<"---------------------------------------------"<<endl;
-	k<<"STT"<<"|"<<"Ma nhan vien"<<"|"<<"Ho ten nhan vien"<<"|"<<"Tuoi"<<"|"<<"Luong"<<endl;
-	k<<"Danh sach sau khi cap nhat : "<<endl;
-	for(int j=0;j<n;j++)
-		k<<a[j].maNv<<"\t"<<" "<<a[j].hoTen<<"\t\t    "<<a[j].tuoi<<"\t"<<a[j].luongCb<<endl;
-	inDanhSachNv(a,n);
+	if(a[index].maNv==x)
+	{
+		fflush(stdin);
+		cout<<"Ho ten nhan vien : ";	cin.getline(a[index].hoTen,30);
+		cout<<"Tuoi : ";	cin>>a[index].tuoi;
+		cout<<"Luong : ";	cin>>a[index].luongCb;
+		cout<<"Danh sach nhan vien sau khi cat nhap"<<endl;
+		k<<"---------------------------------------------"<<endl;
+		k<<"STT"<<"|"<<"Ma nhan vien"<<"|"<<"Ho ten nhan vien"<<"|"<<"Tuoi"<<"|"<<"Luong"<<endl;
+		k<<"Danh sach sau khi cap nhat : "<<endl;
+		for(int j=0;j<n;j++)
+			k<<a[j].maNv<<"\t"<<" "<<a[j].hoTen<<"\t\t    "<<a[j].tuoi<<"\t"<<a[j].luongCb<<endl;
+		inDanhSachNv(a,n);
+	}
+	else
+		cout<<"Ma nhan vien khong ton tai!"<<endl;
 }
 void timKiemNv(nhanVien a[],int n)
 {
@@ -109,15 +133,18 @@ void timKiemNv(nhanVien a[],int n)
 	for(int i=0;i<n;i++)
 	{
 		if(a[i].maNv==x)
-		{
 			index=i;
-			cout<<a[i].maNv<<"\t"<<a[i].hoTen<<"\t"<<a[i].tuoi<<"\t"<<a[i].luongCb<<endl;
-			k<<"---------------------------------------------"<<endl;
-			k<<"STT"<<"|"<<"Ma nhan vien"<<"|"<<"Ho ten nhan vien"<<"|"<<"Tuoi"<<"|"<<"Luong"<<endl;
-			k<<"Ket qua sau khi tim kiem : "<<endl;
-			k<<a[i].maNv<<"\t"<<a[i].hoTen<<"\t"<<a[i].tuoi<<"\t"<<a[i].luongCb<<endl;
-		}
 	}
+	if(a[index].maNv==x)
+	{
+		cout<<a[index].maNv<<"\t"<<a[index].hoTen<<"\t"<<a[index].tuoi<<"\t"<<a[index].luongCb<<endl;
+		k<<"---------------------------------------------"<<endl;
+		k<<"STT"<<"|"<<"Ma nhan vien"<<"|"<<"Ho ten nhan vien"<<"|"<<"Tuoi"<<"|"<<"Luong"<<endl;
+		k<<"Ket qua sau khi tim kiem : "<<endl;
+		k<<a[index].maNv<<"\t"<<a[index].hoTen<<"\t"<<a[index].tuoi<<"\t"<<a[index].luongCb<<endl;
+	}
+	else
+		cout<<"Ma nhan vien khong ton tai!"<<endl;
 }
 void ghifile(char *file,nhanVien a[],int n)
 {
@@ -125,7 +152,7 @@ void ghifile(char *file,nhanVien a[],int n)
 	f.open(file);
 	f<<n<<endl;
 	for(int i=0;i<n;i++)
-		f<<a[i].maNv<<" "<<a[i].hoTen<<" "<<a[i].tuoi<<" "<<a[i].luongCb<<endl;
+		f<<a[i].maNv<<endl<<a[i].hoTen<<endl<<a[i].tuoi<<endl<<a[i].luongCb<<endl;
 	f.close();
 }
 /*void filenhiphan(char *file,nhanVien a[])
@@ -160,15 +187,16 @@ int main()
 		do
 		{
 			system("cls");
+			cout<<"--------Chuong trinh quan ly nhan vien---------"<<endl;
 			cout<<"1.Tao lai danh sach nhan vien"<<endl;
 			cout<<"2.Them mot nhan vien"<<endl;
-			cout<<"3.Sua doi thong tin nhan vien"<<endl;
-			cout<<"4.Xoa mot nhan vien"<<endl;
-			cout<<"5.Tim kiem nhan vien"<<endl;
+			cout<<"3.Sua doi thong tin nhan vien theo ma nhan vien"<<endl;
+			cout<<"4.Xoa mot nhan vien theo ma nhan vien"<<endl;
+			cout<<"5.Tim kiem nhan vien theo ma nhan vien"<<endl;
 			cout<<"6.In danh sach nhan vien"<<endl;
 			cout<<"7.Thoat"<<endl;
-			cout<<"------------------------------"<<endl;
-			cout<<"Chon menu (1->5)"<<endl;
+			cout<<"-----------------------------------------------"<<endl;
+			cout<<"Chon menu (1->7)";
 			cin>>chon;
 			switch(chon)
 			{
@@ -212,15 +240,16 @@ int main()
 		do
 		{
 			system("cls");
-			cout<<"1.Nhap danh sach nhan vien"<<endl;
+			cout<<"--------Chuong trinh quan ly nhan vien---------"<<endl;
+			cout<<"1.Tao lai danh sach nhan vien"<<endl;
 			cout<<"2.Them mot nhan vien"<<endl;
-			cout<<"3.Sua doi thong tin nhan vien"<<endl;
-			cout<<"4.Xoa mot nhan vien"<<endl;
-			cout<<"5.Tim kiem nhan vien"<<endl;
+			cout<<"3.Sua doi thong tin nhan vien theo ma nhan vien"<<endl;
+			cout<<"4.Xoa mot nhan vien theo ma nhan vien"<<endl;
+			cout<<"5.Tim kiem nhan vien theo ma nhan vien"<<endl;
 			cout<<"6.In danh sach nhan vien"<<endl;
 			cout<<"7.Thoat"<<endl;
-			cout<<"------------------------------"<<endl;
-			cout<<"Chon menu (1->5)"<<endl;
+			cout<<"-----------------------------------------------"<<endl;
+			cout<<"Chon menu (1->7)";
 			cin>>chon;
 			switch(chon)
 			{
@@ -233,15 +262,16 @@ int main()
 					do
 					{
 						system("cls");
+						cout<<"--------Chuong trinh quan ly nhan vien---------"<<endl;
 						cout<<"1.Tao lai danh sach nhan vien"<<endl;
 						cout<<"2.Them mot nhan vien"<<endl;
-						cout<<"3.Sua doi thong tin nhan vien"<<endl;
-						cout<<"4.Xoa mot nhan vien"<<endl;
-						cout<<"5.Tim kiem nhan vien"<<endl;
+						cout<<"3.Sua doi thong tin nhan vien theo ma nhan vien"<<endl;
+						cout<<"4.Xoa mot nhan vien theo ma nhan vien"<<endl;
+						cout<<"5.Tim kiem nhan vien theo ma nhan vien"<<endl;
 						cout<<"6.In danh sach nhan vien"<<endl;
 						cout<<"7.Thoat"<<endl;
-						cout<<"------------------------------"<<endl;
-						cout<<"Chon menu (1->5)"<<endl;
+						cout<<"-----------------------------------------------"<<endl;
+						cout<<"Chon menu (1->7)";
 						cin>>chon;
 						switch(chon)
 						{
@@ -282,7 +312,7 @@ int main()
 				case 7:
 					break;
 				default:
-					cout<<"Du lieu trong file khong ton tai!"<<endl;
+					cout<<"Du lieu khong ton tai!"<<endl;
 					system("pause");
 			}
 		}while(chon!=7);
