bf45c289dcac1055506c011706bddf0544901052
TranDangKimKhue <46096987+TranDangKimKhue@users.noreply.github.com>
2019-01-11 21:48:03 +0700

    Update Khue58131328_Bai5.cpp

--- Tuan 3 HTTT/Bai5_58131328/Khue58131328_Bai5.cpp
+++ Tuan 3 HTTT/Bai5_58131328/Khue58131328_Bai5.cpp
@@ -1,109 +1,93 @@
 #include<iostream>
-#include<string>
-#include<fstream>
 using namespace std;
-void LRS(string s)
+#include<string.h>
+#include<fstream>
+void LRS(char X[],int n);
+void inKetQua(char X[],int a[][255],int n);
+void LRS(char X[],int n)
 {
-    int n=s.length();
-    int LV[n+1][n+1];	//Luu vet
-    int TS[n+1][n+1];	//Trong so
-    
-    for(int i=0;i<=n;i++)
-    {
-        for(int j=0;j<=n;j++)
-        {
-        	LV[i][j]=0;
-        	TS[i][j]=0;
+	int TS[n+1][n+1],LV[n+1][n+1];
+	//khoi tai mang TRONG SO va LUU VET
+	for(int i=0;i<=n;i++)
+	{
+		for(int j=0;j<=n;j++)
+		{
+			TS[i][j]=0;
+			LV[i][j]=0;
 		}
 	}
-	
-	for(int i=0;i<=n;i++)
-		cout<<" "<<s[i];
-	cout<<endl;
-	for(int j=0;j<=n;j++)
-		cout<<s[j]<<endl;
-	cout<<endl;
-	//Gan trong so va luu vet vao mang
-    for(int i=1;i<=n;i++)
-    {
-        for(int j=1;j<=n;j++)
-        {
-            if (s[i-1]==s[j-1]&&i!= j)
-            {
-            	TS[i][j]=TS[i-1][j-1]+1;
-            	LV[i][j]=i;
-			}
-            else
-            {
-            	if(TS[i][j-1]>TS[i-1][j])
-            		TS[i][j]=TS[i][j-1];
-            	else
-            		TS[i][j]=TS[i-1][j];
-			}
-			cout<<" "<<TS[i][j];
-        }
-        cout<<endl;
-    }
-    cout<<"Ma tran luu vet : "<<endl;
-    for(int i=1;i<=n;i++)
-    {
-    	for(int j=1;j<=n;j++)
-			cout<<" "<<LV[i][j];
-		cout<<endl;
-	}
-	//tim max tren duong cheo phu tai vi tri cua mang LV[i][j]!=0
-	cout<<"Chuoi tim duoc la : ";
-	for(int k=1;k<=n;k++)
+	//LRS
+	for(int i=1;i<=n;i++)
 	{
-		for(int h=1;h<=n;h++)
+		for(int j=1;j<=n;j++)
 		{
-			if(LV[k][h]!=0)
+			if(X[i-1]==X[j-1]&&i==j)
 			{
-				int max=LV[k][h];
-				if(max<LV[k+1][h-1])
+				TS[i][j]=TS[i][j-1];
+				LV[i][j]=2;
+			}
+			else if(X[i-1]==X[j-1]&&i!=j)
+			{
+				TS[i][j]=TS[i-1][j-1]+1;
+				LV[i][j]=1;
+			}
+			else
+			{
+				if(TS[i][j-1]>TS[i-1][j])
 				{
-					max=LV[k+1][h-1];
-					k++;
-					h--;
+					TS[i][j]=TS[i][j-1];
+					LV[i][j]=2;
 				}
 				else
 				{
-					while(k<n&&h>0)
-					{
-						if(max<LV[k+1][h-1])
-							max=LV[k+1][h-1];
-						k++;
-						h--;
-					}
+					TS[i][j]=TS[i-1][j];
+					LV[i][j]=3;
 				}
-				int tam=max;
-				cout<<s[tam-1];
 			}
 		}
 	}
-	cout<<endl;
-	cout<<"Chuoi lap lai dai nhat la : "<<TS[n][n];
-}
-int main() 
-{
-    string s;
-    ifstream g;
-    g.open("input5.txt");
-    if(g!=NULL)
-    {
-    	getline(g,s);
-    	cout<<"Du lieu tu file : "<<s<<endl;
-    	LRS(s);
-	}
-	else
+	cout<<"Ma tran trong so"<<endl;
+	for(int i=0;i<=n;i++)
 	{
-		cout<<"Nhap du lieu cho chuoi X : ";	getline(cin,s);
-		LRS(s);
-	    ofstream f;
-	    f.open("input5.txt");
-	    f<<s;
-	   f.close();
+		for(int j=0;j<=n;j++)
+			cout<<" "<<TS[i][j];
+		cout<<endl;
 	}
-	g.close();
-    return 0;
+	cout<<"Ma tran luu vet"<<endl;
+	for(int i=0;i<=n;i++)
+	{
+		for(int j=0;j<=n;j++)
+			cout<<" "<<LV[i][j];
+		cout<<endl;
+	}
+	//truy vet
+	int a[255][255];
+	int i=n,j=n;
+	int itam=0;
+	char tam[255];
+	while(i>0&&j>0)
+	{
+		if(LV[i][j]==1)
+		{
+			cout<<X[i-1];
+			i--;
+			j--;
+		}
+		else if(LV[i][j]==2)
+		{
+			j--;
+		}
+		else
+		{
+			i--;
+		}
+	}
+}
+int main()
+{
+	char X[255];
+	cout<<"X : ";	cin.getline(X,255);
+	int n=strlen(X);
+	LRS(X,n);
+	return 0;
 }
